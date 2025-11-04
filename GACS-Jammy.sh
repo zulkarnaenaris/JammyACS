bd10f1dba9bc0c9ed66f19fcd3ac318bdc907c8b
zulkarnaenaris <51742911+zulkarnaenaris@users.noreply.github.com>
2025-11-04 12:03:12 +0800

    Update GACS-Jammy.sh

--- GACS-Jammy.sh
+++ GACS-Jammy.sh
@@ -51,7 +51,7 @@ print_banner() {
 	echo "                                               |_|        "
 	echo ""
 	echo "                  --- Ubuntu 22.04 ---"
-	echo "                  --- AchmadPR ---"
+	echo "                  --- KEBUNTALO NET ---"
 	echo -e "${NC}"
 }
 
